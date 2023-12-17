f872b204ee82d36f91dfe40ed906e45285c23486
komekovv <kakamyratkomekov@gmail.com>
2023-12-18 00:02:32 +0500

    #1 Init application

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <Button>Click</Button>
+    </div>
+  )
+}
+
+export default Page;
